4b77dc46b79ec2b4c513917ffc9e2715676a8236
Yashgamer-x <yashdesai121212@gmail.com>
2026-03-29 00:58:37 -0400

    Optimized imports

--- app/src/main/java/com/unh/communityhelp/mainmenu/view/HomeView.kt
+++ app/src/main/java/com/unh/communityhelp/mainmenu/view/HomeView.kt
@@ -25,7 +25,6 @@ import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
